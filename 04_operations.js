1a86a6e314bc19b738d76aecd3658905e4610773
ayush931 <ayushkumar9315983@gmail.com>
2024-08-15 08:27:41 +0530

    operations

---
+++ 04_operations.js
@@ -0,0 +1,24 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " ayush"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(1 + "2"); // 12
+console.log("1" + 2); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "3"); // 3
+
+console.log(+true); // 1
+console.log(+"");
+
+let  num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
