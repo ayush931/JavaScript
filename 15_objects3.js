2ca546b6ec10bb30b0ef0ba4793882f54c764467
ayush931 <ayushkumar9315983@gmail.com>
2024-08-19 12:23:39 +0530

    Object 3

---
+++ 15_objects3.js
@@ -0,0 +1,11 @@
+const course = {
+    courseName: "JS in Hindi",
+    price: 999,
+    courseInstructor: "Hitesh"
+}
+
+console.log(course.price);
+
+const {courseInstructor: instructor} = course
+console.log(instructor);
+
