41db7d81c477b2061137f65f8ee0796af306fb7e
ayush931 <ayushkumar9315983@gmail.com>
2024-08-19 18:41:54 +0530

    Scope 01

---
+++ 18_scope.js
@@ -0,0 +1,18 @@
+let a = 10
+const b = 20
+var c = 30
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+var f = 60
+
+if (true) {
+    let d = 30
+    const e = 40
+    var f = 50
+}
+// console.log(d)
+// console.log(e)
+console.log(f)
