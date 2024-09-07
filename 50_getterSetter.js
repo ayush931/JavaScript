ceb150d8292268d1b7dfbaa9c7fe2934ac6c2507
ayush931 <ayushkumar9315983@gmail.com>
2024-09-07 20:39:42 +0530

    Getter and Setter

---
+++ 50_getterSetter.js
@@ -0,0 +1,22 @@
+class User {
+    constructor(email, password) {
+        this.email = email
+        this.password = password
+    }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value
+    }
+    get password(){
+        return `${this._password}ayush`
+    }
+    set password(value){
+        this._password = value
+    }
+}
+
+const ayush = new User("a@ayush.ai", "abc")
+console.log(ayush.password)
+console.log(ayush.email)
